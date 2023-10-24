85e21d2fad554e469de1d6460868582fbc6d97ce
Georgy Levin <g.phyzy@gmail.com>
2023-10-24 21:21:53 +0300

    for(const key in json) in txtFillList

--- src/txtFillList.js
+++ src/txtFillList.js
@@ -2,17 +2,23 @@ import { TxtViewer } from "./ui/TxtViewer.js";
 
 const listTxt = document.querySelector(".list-txt");
 
-function fillListTxt() {
-    fetch("/src/txt.json")
-        .then(res => res.json())
-        .then(j => Object.entries(j))
-        .then(ar => ar.map(arObj => `<div class="${arObj[0]}">${fillDivTxt(arObj[1])}</div>`))
-        .then(arS => arS.join(''))
-        .then(s => listTxt.innerHTML = s)
-        .finally(() => new TxtViewer());
+async function fillListTxt() {
+    listTxt.innerHTML  =  await fetch("/src/txt.json")
+                    .then(res => res.json())
+                    .then(json => fillDivs(json));
+
+        new TxtViewer();
 }
 
-function fillDivTxt(arObj) {
+function fillDivs(json) {
+    let s ='';
+    for(const key in json) {
+        s += `<div class="${key}">${fillSpans(json[key])}</div>`;
+    }
+    return s;
+}
+
+function fillSpans(arObj) {
     return arObj.map(e => `<span class="poetry" for-click="${e.file}">${e.name}</span><br>`).join('');
 }
 
